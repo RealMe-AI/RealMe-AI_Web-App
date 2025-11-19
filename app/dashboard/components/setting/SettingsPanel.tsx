e4936946930f900151a609e1bf70a9f6ad5d0210
Owens01 <agunwachidichikere@gmail.com>
2025-11-19 15:54:34 +0100

    added few things

--- app/dashboard/components/setting/SettingsPanel.tsx
+++ app/dashboard/components/setting/SettingsPanel.tsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { X, Globe, Bell, Trash2, Edit2 } from "lucide-react";
 import { useSettings } from "../../../hooks/useSettings";
 import { useUserStore } from "../../../zustand/useUserStore";
-import { useThemeStore } from "../../../zustand/useThemeStore"; // ✅ added
+import { useThemeStore } from "../../../zustand/useThemeStore";
 import EditProfileModal from "./EditProfileModal";
 import CustomSelect from "./CustomSelect";
 
