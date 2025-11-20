422cb08fd4fefe6e6163f258df72055bddec8d0a
Owens01 <agunwachidichikere@gmail.com>
2025-11-20 19:06:57 +0100

    deleted a text

--- app/components/SplashScreen.tsx
+++ app/components/SplashScreen.tsx
@@ -22,7 +22,7 @@ export default function SplashScreen({
         <motion.div
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 z-50 flex items-center justify-center bg-white"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-white dark:bg-slate-600"
         >
           <motion.div
             aria-hidden
