09095b03a1e7f603815a4d065193957f5583a946
Owens01 <agunwachidichikere@gmail.com>
2025-11-22 13:55:03 +0100

    added next-intl.config.ts

---
+++ next-intl.config.ts
@@ -0,0 +1,14 @@
+// next-intl.config.ts
+// No types needed
+const nextIntlConfig = {
+  // default locale for your app
+  defaultLocale: "en",
+
+  // supported locales
+  locales: ["en", "ha", "ig", "yo"],
+
+  // optional: path to translation files
+  messagesDirectory: "app/i18n",
+};
+
+export default nextIntlConfig;
