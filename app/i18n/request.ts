ce48d3e4b199508401d7ddf1feb181abdbb2b334
Owens01 <agunwachidichikere@gmail.com>
2025-11-22 17:40:37 +0100

    updated a file

--- app/i18n/request.ts
+++ app/i18n/request.ts
@@ -1,19 +1,19 @@
-// app/i18n/getRequestConfig.ts
-import { getRequestConfig } from "next-intl/server";
-import { notFound } from "next/navigation";
+// // app/i18n/getRequestConfig.ts
+// import { getRequestConfig } from "next-intl/server";
+// import { notFound } from "next/navigation";
 
-// Supported locales
-const locales = ["en", "ha", "ig", "yo"];
+// // Supported locales
+// const locales = ["en", "ha", "ig", "yo"];
 
-export default getRequestConfig(async ({ locale }) => {
-  // Ensure locale is defined
-  if (!locale || !locales.includes(locale)) notFound();
+// export default getRequestConfig(async ({ locale }) => {
+//   // Ensure locale is defined
+//   if (!locale || !locales.includes(locale)) notFound();
 
-  // TypeScript now knows locale is a string
-  const messages = (await import(`./${locale}.ts`)).default;
+//   // TypeScript now knows locale is a string
+//   const messages = (await import(`./${locale}.ts`)).default;
 
-  return {
-    locale,   // definitely a string here
-    messages, // translation messages
-  };
-});
+//   return {
+//     locale,   // definitely a string here
+//     messages, // translation messages
+//   };
+// });
