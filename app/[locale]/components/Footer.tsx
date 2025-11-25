0e3d537d26f5da096c80b42432b26a3c7fddab95
Owens01 <agunwachidichikere@gmail.com>
2025-11-25 14:28:00 +0100

    updated a file

--- app/[locale]/components/Footer.tsx
+++ app/[locale]/components/Footer.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
-  const t = useTranslations();
+  const t = useTranslations("landing.footer");
 
   return (
     <footer className="bg-slate-900 text-slate-200">
       <div className="border-t border-slate-800 text-slate-500 text-sm py-6">
         <div className="max-w-7xl mx-auto px-6 flex justify-between">
           <div>
-            © {new Date().getFullYear()} RealMe AI. {t("footer.")}. @OwenVisuels
+            © {new Date().getFullYear()} RealMe AI. {t("rights")}. @OwenVisuels
           </div>
 
           <div className="flex gap-4">
-            <Link href={"/"}>{t("landing.footer.privacy")}</Link>
-            <Link href={"/"}>{t("landing.footer.terms")}</Link>
-            <Link href={"/"}>{t("landing.footer.contact")}</Link>
+            <Link href={"/"}>{t("privacy")}</Link>
+            <Link href={"/"}>{t("terms")}</Link>
+            <Link href={"/"}>{t("contact")}</Link>
           </div>
         </div>
       </div>
