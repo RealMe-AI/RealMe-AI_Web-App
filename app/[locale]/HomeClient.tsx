86f27991c12cb7cb7ba24aaee077cc67000d2c72
Owens01 <agunwachidichikere@gmail.com>
2025-11-25 16:17:13 +0100

    updated a HomeClient.tsx

--- app/[locale]/HomeClient.tsx
+++ app/[locale]/HomeClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import SplashScreen from "../slash-screen/SplashScreen";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import GetStartedSection from "./components/GetStartedSection";
@@ -13,7 +13,6 @@ export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
   const [active] = useState<string>("home");
-  
 
   return (
     <>
@@ -23,6 +22,8 @@ export default function Home() {
       />
       {!showSplash && (
         <div className="min-h-screen flex flex-col">
+          <div className="bg-red-500 text-white p-4">NAV TEST</div>
+
           <Navbar isOpen={isOpen} setIsOpen={setIsOpen} active={active} />
           <main className="flex-1">
             <Hero />
