8836450d5589295f89c7c89c048b845c13dfbfa1
Owens01 <agunwachidichikere@gmail.com>
2025-11-26 20:09:56 +0100

    updated a file

--- app/[locale]/dashboard/components/ProfileFooter.tsx
+++ app/[locale]/dashboard/components/ProfileFooter.tsx
@@ -98,7 +98,7 @@ export default function ProfileFooter() {
 
       {/* Footer Text */}
       <p className="text-[10px] text-center text-slate-500 dark:text-slate-500 mt-3">
-        {t("dashboard.sidebar.footer_full")}
+        OwensVisuels {t("dashboard.sidebar.footer_full")}
       </p>
 
       {/* Account Info Modal */}
