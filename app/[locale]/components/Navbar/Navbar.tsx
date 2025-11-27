408334cfa256a337e6e555272042c03cb58b9cb6
Owens01 <agunwachidichikere@gmail.com>
2025-11-27 15:15:35 +0100

    updated a file

--- app/[locale]/components/Navbar/Navbar.tsx
+++ app/[locale]/components/Navbar/Navbar.tsx
@@ -36,15 +36,17 @@ export default function Navbar({ isOpen, setIsOpen, active }: Props) {
         {/* Desktop Navigation */}
         <DesktopNav active={active} />
 
-        <div>
+        <div className="flex items-center 
+        
+        ">
           {/* Theme Toggle */}
           <motion.button
             onClick={toggleTheme}
             whileTap={{ rotate: 180 }}
             transition={{ duration: 0.4 }}
-            className="ml-4 p-2 rounded-full bg-gray-100 dark:bg-slate-700 hover:bg-gray-200 dark:hover:bg-slate-600 text-slate-700 dark:text-gray-100"
+            className=" p-2 rounded-full bg-gray-100 dark:bg-slate-700 hover:bg-gray-200 dark:hover:bg-slate-600 text-slate-700 dark:text-gray-100"
           >
-            {theme === "light" ? <Sun size={18} /> : <Moon size={18} />}
+            {theme === "light" ? <Sun size={16} /> : <Moon size={16} />}
           </motion.button>
 
           {/* Mobile Menu Button */}
