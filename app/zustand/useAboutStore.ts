1a1ac3128a12524a8b891ad2b5f8330c3d1d2fea
Owens01 <agunwachidichikere@gmail.com>
2025-11-27 22:14:05 +0100

    updated a file

--- app/zustand/useAboutStore.ts
+++ app/zustand/useAboutStore.ts
@@ -1,7 +1,10 @@
 import { create } from "zustand";
+import { type AboutStore } from "../types/type";
 
-export const useAboutStore = create((set) => ({
-  showAbout: false,
-  openAbout: () => set({ showAbout: true }),
-  closeAbout: () => set({ showAbout: false }),
+
+export const useAboutStore = create<AboutStore>((set) => ({
+   isOpen: false,
+
+  openAbout: () => set({ isOpen: true }),
+  closeAbout: () => set({ isOpen: false }),
 }));
