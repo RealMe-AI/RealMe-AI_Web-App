293fdb15bf3aea96520d53edf6e5fd5c7655538f
Owens01 <agunwachidichikere@gmail.com>
2025-11-28 17:44:15 +0100

    updated a file

--- app/[locale]/components/Hero.tsx
+++ app/[locale]/components/Hero.tsx
@@ -2,7 +2,6 @@
 import { motion } from "framer-motion";
 import { CheckCircle2, Languages, Brain } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { Link } from "@/i18n/routing";
 
 import Image from "next/image";
 import CTAButtons from "./CTAButtons";
