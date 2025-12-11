480337c3a7d1c8d55c9a6d7d18183608d35c1594
Owens01 <agunwachidichikere@gmail.com>
2025-12-11 18:20:12 +0100

    updated a file

--- app/[locale]/auth/page.tsx
+++ app/[locale]/auth/page.tsx
@@ -23,6 +23,7 @@ export default function AuthPage() {
           <ArrowLeft className="w-4 h-4" /> {t("auth.page.back_button")}
         </button>
 
+        {/* Auth Card */}
         <motion.div
           initial={{ y: 30, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
