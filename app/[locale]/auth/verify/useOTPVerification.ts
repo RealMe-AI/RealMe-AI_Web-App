e29b8d1b43531e5e23dfedb51e80b51fb2b245e4
Owens01 <agunwachidichikere@gmail.com>
2025-12-21 20:05:13 +0100

    updated a file

--- app/[locale]/auth/verify/useOTPVerification.ts
+++ app/[locale]/auth/verify/useOTPVerification.ts
@@ -106,7 +106,7 @@ export function useOTPVerification() {
 
       if (!res.ok) throw new Error("Failed to resend OTP");
 
-      // Reset UI 
+      // Reset UI + timer
       setOtp(["", "", "", "", "", ""]);
       setInvalidCode(false);
       setExpired(false);
