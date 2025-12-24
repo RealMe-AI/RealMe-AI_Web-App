76dd4c2ca768823d04ad36cbc33f33f4e8e7dfc2
Owens01 <agunwachidichikere@gmail.com>
2025-12-24 20:28:28 +0100

    updated a file

--- app/[locale]/dashboard/components/SidebarItem.tsx
+++ app/[locale]/dashboard/components/SidebarItem.tsx
@@ -48,12 +48,18 @@ export default function SidebarItem({
 
       {/* Action button */}
       <button
-        className="
+        className={`
           ml-2 
           text-slate-600 dark:text-slate-300
           opacity-100 md:opacity-0 md:group-hover:opacity-100
           transition-opacity duration-200 text-lg leading-none
-        "
+          ${
+            isActive ?
+            "text-slate-300"
+            :
+           " text-slate-600"
+          }
+        `}
         onClick={(e) => {
           e.stopPropagation();
           setIsMenuOpen(true);
