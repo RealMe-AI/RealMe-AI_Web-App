b1874530273b85077fd60b45677ee73dfdd0ae88
Owens01 <agunwachidichikere@gmail.com>
2026-01-02 09:31:48 +0100

    updated a file

--- app/zustand/useChatStore.ts
+++ app/zustand/useChatStore.ts
@@ -90,7 +90,7 @@ export const useChatStore = create<ChatState>((set, get) => ({
       isLoading: true,
     }));
 
-    /*   from "../lib/baseUrl"; // Ensure this import is added at top */
+    /* "../lib/baseUrl"; // Ensure this import is added at top */
 
     const token = localStorage.getItem("accessToken");
     if (!token) {
