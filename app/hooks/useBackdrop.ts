2a9c597b2f94f2e377040a3d48b24a3524576330
Owens01 <agunwachidichikere@gmail.com>
2026-01-31 13:53:01 +0100

    updated a file

--- app/hooks/useBackdrop.ts
+++ app/hooks/useBackdrop.ts
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 /**
  * useBackdrop Hook
  
- * @param isOpen - Whether the backdrop 
+ * @param isOpen - Whether the  
  */
 export function useBackdrop(isOpen: boolean) {
   useEffect(() => {
