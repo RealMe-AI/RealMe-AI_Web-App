9ec7d6f1171690acad4ea8e4b85cd1737dcedc31
Owens01 <agunwachidichikere@gmail.com>
2026-02-13 18:30:09 +0100

    updated a file

--- app/[locale]/setting/SettingsPanel.tsx
+++ app/[locale]/setting/SettingsPanel.tsx
@@ -39,7 +39,7 @@ export default function SettingsPanel({ open, close }: SettingsPanelProps) {
               animate={{ y: 0, opacity: 1, scale: 1 }}
               exit={{ y: 50, opacity: 0, scale: 0.95 }}
               transition={{ duration: 0.25 }}
-              className="relative w-[95%] max-w-2xl bg-white/70 dark:bg-slate-800/80 backdrop-blur-xl rounded-2xl p-6 shadow-xl border border-white/20 overflow-y-auto max-h-[90vh]"
+              className="relative w-[95%] max-w-2xl bg-white/70 dark:bg-slate-800/80 backdrop-blur-xl rounded-2xl p-6 shadow-xl border border-white/20 overflow-y-auto lg:overflow-y-hidden max-h-[90vh]"
             >
               <button
                 onClick={close}
