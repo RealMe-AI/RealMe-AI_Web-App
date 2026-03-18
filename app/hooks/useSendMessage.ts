8f471463ebc5c803c6394344b222b21191be87fc
Owens01 <agunwachidichikere@gmail.com>
2026-03-18 17:48:55 +0100

    updated a file

--- app/hooks/useSendMessage.ts
+++ app/hooks/useSendMessage.ts
@@ -21,7 +21,7 @@ export const useSendMessage = () => {
     async (content: string) => {
       if (!content.trim()) return;  
 
-      // Helper function to update conv
+      // Helper function to update conve
       const updateConversationDetails = async (
         conversationId: number,
         lastMessage: string,
